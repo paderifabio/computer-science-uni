191c1664955d2371742c97f01d56156fdd0d43b0
paderifabio <paderifabio@gmail.com>
2021-01-09 09:34:06 +0100

    istruzione switch modified

--- esercizi-libro/capitolo-3/istruzione_switch.java
+++ esercizi-libro/capitolo-3/istruzione_switch.java
@@ -4,4 +4,37 @@ Istruzione switch:
 serve quando si hanno tanti if-else nestati, e può aiutare a 
 rendere più comprensibile il codice
 
- 
+*/
+
+import java.util.Scanner;
+
+class ProvaSwitch {
+      
+      public static void main(String[] args) {
+            
+            int numeroNeonati;
+            System.out.println("Inserisci il numero di nascite:");
+            Scanner keyboard = new Scanner(System.in);
+            numeroNeonati = keyboard.nextInt();
+        
+        switch(numeroNeonati) {
+              
+              case 1:
+                    System.out.println("Un figlio!");
+                    break;
+              
+              case 2:
+                    System.out.println("Due figli!");
+                    break;
+              
+              case 3:
+              
+              default:
+                    System.out.println("Il numero di figli è:"+numeroNeonati);
+                    break;
+              
+        }    
+            
+      }
+      
+}
